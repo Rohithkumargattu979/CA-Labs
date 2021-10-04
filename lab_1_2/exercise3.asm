53ed2a899373e9699ce162e6c7dcf7d2fb40e2fb
Rohith Kumar Gattu <57759452+Rohithkumargattu979@users.noreply.github.com>
2021-10-04 16:01:19 +0530

    Update exercise3.asm

--- lab_1_2/exercise3.asm
+++ lab_1_2/exercise3.asm
@@ -1,42 +1,61 @@
 .data
-nl: .asciiz "\n"
-addMsg: .asciiz "The Sum is: "
-subMsg: .asciiz "The Diff is: "
+inp1: .asciiz "Enter Input 1 : "
+inp2: .asciiz "Enter Input 2 : "
+addd: .asciiz "Addition of given numbers is : "
+subb: .asciiz "Subtraction of given numbers is : "
+newline: .asciiz "\n"
 
 .text
 .globl main
 
 main:
-    # Set Variables 
-    li	$t1, 10		        
-    li	$t2, 15		        
-    
-    la	$a0, addMsg		    
-    li	$v0, 4		        
-    syscall
+	la $a0, inp1 #ask message for input1
+	li $v0, 4 #loading for printing the string
+	syscall # invoking system call
+	
+	li $v0, 5 # reading integer
+	syscall # invoking system call
+	
+	move $t0, $v0 #moving input1 to $t0
 
-    add	$t0, $t1, $t2		
-    addi	$a0, $t0, 0		
-    li	$v0, 1		        
-    syscall
-
-    la	$a0, nl		        
-    li	$v0, 4		        
-    syscall
-
-    la	$a0, subMsg		   
-    li	$v0, 4		        
-    syscall
-
-    sub	$t0, $t1, $t2		
-    addi	$a0, $t0, 0		
-    li		$v0, 1		        
-    syscall
-
-    la		$a0, nl		        
-    li		$v0, 4		        
-    syscall
-
-    li		$v0, 10		        
+	la $a0, inp2 
+	li $v0, 4
 	syscall
-    
+	
+	li $v0, 5
+	syscall
+	
+	move $t1, $v0
+
+	add $t2, $t0, $t1 #adding t0, t1 and storing in t2
+	
+	la $a0, addd # load address to print addition string
+	li $v0, 4 #load 4 to print string
+	syscall
+	
+	move $a0, $t2 #load added value to print it
+	li $v0,1 #load 1 to print integer
+	syscall
+
+	la $a0, newline
+	li $v0, 4
+	syscall
+	
+	sub $t3, $t0, $t1 #subtraction of t0,t1
+	
+	la $a0, subb # load address toprint subtraction string
+	li $v0, 4 #load 4 to print string
+	syscall
+	
+	move $a0, $t3 #load subtracted value to print it
+	li $v0, 1 #load 1 to print integer
+	syscall
+ 
+  	#exit
+	li $v0, 10
+	syscall
+
+
+	#@RohithGattu
+
+	
